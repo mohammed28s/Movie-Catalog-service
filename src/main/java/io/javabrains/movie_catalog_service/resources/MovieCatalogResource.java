e5a5c68321ab0a64864f8ea638bbf5359a985745
Mohammed Salim Al Salhi <mohammed.alsalhi@moh.gov.om>
2026-01-11 09:10:26 +0400

    The moive api has been updated

--- src/main/java/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
+++ src/main/java/io/javabrains/movie_catalog_service/resources/MovieCatalogResource.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/catalog")
+@RequestMapping("/catalog")          // The main path to the catalog API
 public class MovieCatalogResource {
 
 
